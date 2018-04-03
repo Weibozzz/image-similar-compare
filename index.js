bf8c56794b04f658611bb551551be9ad0006a008
15691808595 <739291780@qq.com>
2018-04-03 23:43:11 +0800

    es7的await方法： 循环中如果后一个Promise的执行依赖与前一个Promise的执行结果(即必要等当前Promise执行完了再进行下次循环) 如何让for循环中的Promise全部完成后执行某个操作？还没解决

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function findSync(startPath) {
         });
 
     }
+
     finder(startPath);
     return result;
 }
@@ -27,26 +28,97 @@ let fileNames = findSync('./images');//所有的图片路径
 
 const targetImage = './compare-target.jpg';
 const target = imghash.hash(targetImage);
-
-function compareResult (fileNames) {
+//循环中如果后一个Promise的执行依赖与前一个Promise的执行结果(即必要等当前Promise执行完了再进行下次循环)
+//es6解决方案
+/*function compareResult (fileNames) {
+    let arr = [];
+    let j = 0;
+    function arrPush(compareTarget, compareTargetImage) {
+        return new Promise(resolve => {
+            Promise.all([target, compareTarget]).then((results) => {
+                const dist = hamming(results[0], results[1]);
+                arr.push({
+                    image: compareTargetImage,
+                    similar: dist
+                });
+                resolve(dist);
+            })
+        })
+    }
+    for (let i = 0; i < fileNames.length; i++) {
+        const compareTargetImage = fileNames[i];
+        const compareTarget = imghash.hash(compareTargetImage);
+        arrPush(compareTarget, compareTargetImage).then((dist)=>{
+            if(dist!==undefined){
+                j++;
+                arrPush(compareTarget, compareTargetImage)
+                
+            }
+        })
+    }
+    console.log(j)
+    // return arr;
+}
+compareResult(fileNames)*/
+// es7解决方案
+/*async function compareResult(fileNames) {
     let arr = [];
 
-    for (let i = 0; i < fileNames.length; i++) {
-        ((i)=>{
-            const compareTargetImage = fileNames[i];
-            const compareTarget = imghash.hash(compareTargetImage);
-            Promise
-                .all([target, compareTarget])
-                .then((results) => {
-                    const dist = hamming(results[0], results[1]);
-                    console.log(`${compareTargetImage}Distance between images is: ${dist}`);
-                    arr.push({
-                        image:compareTargetImage,
-                        similar:dist
-                    })
-
-                })
-        })(i)
+    function arrPush(compareTarget, compareTargetImage) {
+        return new Promise(resolve => {
+            Promise.all([target, compareTarget]).then((results) => {
+                const dist = hamming(results[0], results[1]);
+                arr.push({
+                    image: compareTargetImage,
+                    similar: dist
+                });
+                resolve();
+            })
+        })
     }
+
+    for (let i = 0; i < fileNames.length; i++) {
+        const compareTargetImage = fileNames[i];
+        const compareTarget = imghash.hash(compareTargetImage);
+        await arrPush(compareTarget, compareTargetImage)
+    }
+    return arr;
 }
-console.log(compareResult(fileNames))
+compareResult(fileNames).then((arr) => {
+    console.log(arr.sort((a, b) => {
+        return a.similar - b.similar
+    }))
+});*/
+async function compareResult(fileNames) {
+    let arr = [];
+
+    function arrPush(i) {
+        const compareTargetImage = fileNames[i];
+        const compareTarget = imghash.hash(compareTargetImage);
+        console.log(i)
+        return new Promise(resolve => {
+            Promise.all([target, compareTarget]).then((results) => {
+                const dist = hamming(results[0], results[1]);
+                arr.push({
+                    image: compareTargetImage,
+                    similar: dist
+                });
+                resolve();
+            })
+        })
+    }
+
+    new Promise(function(resolve){
+        resolve();
+    }).then(()=>{
+        for (let i = 0; i < fileNames.length; i++) {
+            arrPush(i)
+        }
+    }).then(()=>{
+        console.log(arr)
+        return arr;
+    })
+
+}
+compareResult(fileNames)
+console.log(123)
